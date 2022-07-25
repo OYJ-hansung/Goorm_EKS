72df5ed7abc5c2850c44103a9ade0625908b9c20
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-07-25 19:04:06 +0900

    refactor : vpc subnet 변수화

--- variables.tf
+++ variables.tf
@@ -7,3 +7,14 @@ variable "instance_type" {
   default = "t3.medium"
   type    = string
 }
+
+# VPC
+variable "public_subnet_cidr" {
+  description = "VPC CIDR BLOCK LIST"
+  default = ["172.20.0.0/20", "172.20.16.0/20"]
+}
+
+variable "private_subnet_cidr" {
+  description = "VPC CIDR BLOCK LIST"
+  default = ["172.20.32.0/20", "172.20.48.0/20"]
+}
