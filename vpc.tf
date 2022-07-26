6f62fa33f642c129e979bd51255344f59d7cf6bf
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-07-26 18:58:17 +0900

    refactor: vpc
    1. 변수화
    2. 가용영역 수정
    3. nat gateway 수정

--- vpc.tf
+++ vpc.tf
@@ -1,118 +1,96 @@
 # create VPC
 resource "aws_vpc" "vpc" {
-  cidr_block = "172.20.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
-    Name = "boookk-VPC"
-  }
-}
-
-
-resource "aws_subnet" "public_subnet" {
-  count = length(var.public_subnet_cidr)
-
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = element(var.public_subnet_cidr, count.index)
-  availability_zone = "ap-northeast-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "boookk_public_subent${count.index + 1}"
-  }
-}
-
-/*
-# create VPC
-resource "aws_vpc" "GoormProject-VPC" {
-  cidr_block = "172.20.0.0/16"
-  tags = {
-    Name = "GoormProject-VPC"
+    Name = "${var.vpc_name}"
   }
 }
 
 # create subnet
-resource "aws_subnet" "GoormProject-Public1" {
-  vpc_id            = aws_vpc.GoormProject-VPC.id
-  cidr_block        = "172.20.0.0/20"
-  availability_zone = "ap-northeast-2a"
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnet_cidr)
+
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.public_subnet_cidr, count.index)
+  availability_zone       = element(var.az, count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = "GoormProject-Public1"
+    Name = "${var.vpc_name}-subent-public${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "GoormProject-Public2" {
-  vpc_id            = aws_vpc.GoormProject-VPC.id
-  cidr_block        = "172.20.16.0/20"
-  availability_zone = "ap-northeast-2c"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "GoormProject-Public2"
-  }
-}
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnet_cidr)
 
-resource "aws_subnet" "GoormProject-Pravate1" {
-  vpc_id            = aws_vpc.GoormProject-VPC.id
-  cidr_block        = "172.20.32.0/20"
-  availability_zone = "ap-northeast-2b"
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = element(var.private_subnet_cidr, count.index)
+  availability_zone = element(var.az, count.index)
   tags = {
-    Name = "GoormProject-Private1"
-  }
-}
-
-resource "aws_subnet" "GoormProject-Pravate2" {
-  vpc_id            = aws_vpc.GoormProject-VPC.id
-  cidr_block        = "172.20.48.0/20"
-  availability_zone = "ap-northeast-2d"
-  tags = {
-    Name = "GoormProject-Private2"
+    Name = "${var.vpc_name}-subent-private${count.index + 1}"
   }
 }
 
 # create IGW
-resource "aws_internet_gateway" "IGW" {
-  vpc_id = aws_vpc.GoormProject-VPC.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = "GoormProject-VPC_IGW"
+    Name = "${var.vpc_name}-igw"
   }
 }
 
 # create route table
-resource "aws_route_table" "GoormProject-VPC-Public-route" {
-  vpc_id = aws_vpc.GoormProject-VPC.id
-
+resource "aws_route_table" "vpc_public_route" {
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.IGW.id
+    gateway_id = aws_internet_gateway.igw.id
   }
-
   tags = {
-    "Name" = "GoormProject-VPC-Public-route"
+    "Name" = "${var.vpc_name}-public-route"
   }
 }
 
 # create route table association
 # Subnet 과 Route table 을 연결
-resource "aws_route_table_association" "GoormProject-VPC-Public-routing1" {
-  route_table_id = aws_route_table.GoormProject-VPC-Public-route.id
-  subnet_id      = aws_subnet.GoormProject-Public1.id
+resource "aws_route_table_association" "vpc_public_routing" {
+  count = length(var.public_subnet_cidr)
+
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.vpc_public_route.id
 }
 
-resource "aws_route_table_association" "GoormProject-VPC-Public-routing2" {
-  route_table_id = aws_route_table.GoormProject-VPC-Public-route.id
-  subnet_id      = aws_subnet.GoormProject-Public2.id
+
+# create eip for nat
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  # depends_on = [aws_internet_gateway.igw]
 }
 
-# create NAT GW
-resource "aws_route_table" "GoormProject-VPC-Private-route" {
-  vpc_id = aws_vpc.GoormProject-VPC.id
+# create nat gateway
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+  # depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${var.vpc_name}-nat"
+  }
 }
 
-resource "aws_route_table_association" "GoormProject-VPC-Private-routing1" {
-  route_table_id = aws_route_table.GoormProject-VPC-Private-route.id
-  subnet_id      = aws_subnet.GoormProject-Pravate1.id
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+ }
+  tags = {
+    Name = "${var.vpc_name}-private-route"
+  }
 }
 
-resource "aws_route_table_association" "GoormProject-VPC-Private-routing2" {
-  route_table_id = aws_route_table.GoormProject-VPC-Private-route.id
-  subnet_id      = aws_subnet.GoormProject-Pravate2.id
+resource "aws_route_table_association" "vpc_private_routing" {
+  count = length(var.private_subnet_cidr)
+
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private_route.id
 }
-*/
