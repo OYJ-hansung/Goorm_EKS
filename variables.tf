ff7fa1e8a24038efc8bf724a6e8f0011ea54b0b1
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-07-27 17:31:27 +0900

    feat : add security group
    1. 보안그룹 관련 변수 추가
    2. vpc cidr 수정
    3. 서브넷 cidr 블록 변수 이름 수정

--- variables.tf
+++ variables.tf
@@ -8,7 +8,9 @@ variable "instance_type" {
   type    = string
 }
 
+######
 # VPC
+######
 variable "vpc_name" {
   description = "VPC NAME"
   default = "terraform-vpc"
@@ -16,20 +18,41 @@ variable "vpc_name" {
 
 variable "vpc_cidr" {
   description = "VPC CIDR BLOCK"
-  default = "172.20.0.0/16"
+  # default = "172.20.0.0/16"
+  default = "172.16.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-  description = "VPC CIDR BLOCK LIST"
-  default = ["172.20.0.0/20", "172.20.16.0/20"]
+variable "public_subnet_cidr_blocks" {
+  description = "List of pulic subnet cidr block"
+  # default = ["172.20.0.0/20", "172.20.16.0/20"]
+  default = ["172.16.0.0/20", "172.16.16.0/20"]
 }
 
-variable "private_subnet_cidr" {
-  description = "VPC CIDR BLOCK LIST"
-  default = ["172.20.32.0/20", "172.20.48.0/20"]
+variable "private_subnet_cidr_blocks" {
+  description = "List of private subnet cidr block"
+  # default = ["172.20.32.0/20", "172.20.48.0/20"]
+  default = ["172.16.32.0/20", "172.16.48.0/20"]
 }
 
 variable "az" {
-  description = "AZ of public subnet"
+  description = "List of availability zone"
   default = ["ap-northeast-2a", "ap-northeast-2c"]
+}
+
+#################
+# Security Group
+#################
+variable "sg_eks_cluster_name" {
+  description = "Name of security group"
+  default = "sg_eks_cluster"
+}
+
+variable "sg_eks_cluster_description" {
+  description = "Description of security group"
+  default = "Security group"
+}
+
+variable "sg_eks_cluster_ingress_port" {
+  description = "List of ingress port on security group"
+  default = [443]
 }
