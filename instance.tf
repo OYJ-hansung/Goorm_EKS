f65448635649215cd6aa9ac1f53b0958473fbc74
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-07-27 18:54:58 +0900

    refactor : instance 변수화

--- instance.tf
+++ instance.tf
@@ -1,22 +1,24 @@
 data "aws_key_pair" "buildsvr_key"{
-    include_public_key = true
-    filter {
-        name   = "tag:key"
-        values = ["key"]
-    }
+  include_public_key = true
+
+  filter {
+      name   = "tag:${var.key_pair_tag_key}"
+      values = [var.key_pair_tag_value]
+  }
 }
 
 resource "aws_instance" "buildsvr" {
-  ami           = "ami-0ea5eb4b05645aa8a" # ap-northeast-2 Ubuntu Server 20.04 LTS x64(x86)
-  instance_type = "t2.medium" # 인스턴스 유형
-  key_name      = data.aws_key_pair.buildsvr_key.key_name 
-  availability_zone = aws_subnet.GoormProject-Public1.availability_zone # Worker Node와 동일한 VPC, AZ
+  ami               = var.ami
+  instance_type     = var.instance_type
+  key_name          = data.aws_key_pair.buildsvr_key.key_name
+  availability_zone = aws_subnet.public_subnet[0].availability_zone # Worker Node와 동일한 VPC, AZ
+
   associate_public_ip_address = true # IP 자동 부여 활성화
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = "10"
-    volume_type = "gp2"
+
+  root_block_device {
+    volume_size = var.instance_size
   }
+
   user_data = <<-EOF
                 #!/bin/bash
                 sudo mkdir /var/lib/jenkins
@@ -24,8 +26,9 @@ resource "aws_instance" "buildsvr" {
                 sudo apt-get update -y && sudo apt-get install -y openjdk-11-jre
                 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
                 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-                sudo apt-get update -y && sudo apt-get install jenkins -y         
+                sudo apt-get update -y && sudo apt-get install jenkins -y
                 EOF
+
   tags = {
     Name = "Build Server"
   }
