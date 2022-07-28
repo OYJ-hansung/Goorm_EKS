23de3cc7d67a8503e578aa58f8cbb0822ad3b85e
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-07-28 18:35:35 +0900

    refactor : worker_nodes.tf 변수화

--- worker_nodes.tf
+++ worker_nodes.tf
@@ -1,29 +1,25 @@
 # Create Node Group
-resource "aws_eks_node_group" "eks-nodes-t2" {
-  cluster_name    = aws_eks_cluster.GoormEKSCluster.name
-  node_group_name = "eks-nodes-t2"
-  subnet_ids      = [aws_subnet.GoormProject-Public1.id]
-  node_role_arn   = aws_iam_role.Goorm-eks-nodes-role.arn
-  instance_types  = ["t2.medium"]
-  disk_size = 20
+resource "aws_eks_node_group" "eks_nodes_t2" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = var.eks_node_group_name
+  subnet_ids      = [aws_subnet.public_subnet[0].id]
+  node_role_arn   = aws_iam_role.eks_node.arn
+  instance_types  = var.eks_nodes_instance_types
+  disk_size       = var.eks_nodes_disk_size
 
-  labels = {
-    "role" = "eks-t2-medium"
-  }
+  labels = var.eks_node_labels
 
   scaling_config {
-    desired_size = 2
-    min_size     = 2
-    max_size     = 4
+    desired_size = var.eks_node_sc_desired_size
+    min_size     = var.eks_node_sc_min_size
+    max_size     = var.eks_node_sc_max_size
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks-node-EKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.eks-node-EKS_CNI_Policy,
-    aws_iam_role_policy_attachment.eks-node-EC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.eks_node_policy,
   ]
 
   tags = {
-    "Name" = "${aws_eks_cluster.GoormEKSCluster.name}-eks-t2-medium-Node"
+    "Name" = "${aws_eks_cluster.eks_cluster.name}-eks-t2-medium-Node"
   }
 }
