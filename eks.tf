217686f3b470373b52dd7219699de99a1940d3d0
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-07-28 18:27:03 +0900

    refactor : eks.tf 변수화

--- eks.tf
+++ eks.tf
@@ -1,20 +1,21 @@
+#################
 # create cluster
-resource "aws_eks_cluster" "GoormEKSCluster" {
-  name = "GoormEKSCluster"
-  role_arn = aws_iam_role.GoormEKSRole.arn
-  version  = "1.22"
+#################
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = var.cluster_version
 
   # enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   vpc_config {
-    security_group_ids      = [aws_security_group.GoormEKSClusterSG.id]
-    subnet_ids              = [aws_subnet.GoormProject-Public1.id, aws_subnet.GoormProject-Pravate1.id]
-    endpoint_private_access = true
-    endpoint_public_access  = true
+    security_group_ids      = [aws_security_group.sg_eks_cluster.id]
+    subnet_ids              = aws_subnet.public_subnet.*.id
+    endpoint_private_access = var.cluster_endpoint_private_access
+    endpoint_public_access  = var.cluster_endpoint_public_access
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks-cluster-EKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks-cluster-EKSVPCResourceController,
+    aws_iam_role_policy_attachment.eks_cluster_policy
   ]
 }
