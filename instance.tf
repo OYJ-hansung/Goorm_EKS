86cf68b04e306d92fb2124b1a46618f7b7d70141
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-07-29 15:51:45 +0900

    feat : add docker engine
    and add jenkins user to docker group

--- instance.tf
+++ instance.tf
@@ -27,6 +27,21 @@ resource "aws_instance" "buildsvr" {
                 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
                 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
                 sudo apt-get update -y && sudo apt-get install jenkins -y
+                sudo apt-get update
+                sudo apt-get install ca-certificates curl gnupg lsb-release
+                sudo mkdir -p /etc/apt/keyrings
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                echo deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt-get update
+                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+                apt-cache madison docker-ce
+                sudo apt-get install docker-ce=5:20.10.16~3-0~ubuntu-focal docker-ce-cli=5:20.10.16~3-0~ubuntu-focal containerd.io docker-compose-plugin -y
+                sudo gpasswd -a jenkins docker
+                sudo systemctl daemon-reload
+                sudo systemctl restart docker
+                sudo service jenkins restart
+                curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+                sudo mv kustomize /usr/local/bin
                 EOF
 
   tags = {
