83953f33d2f75b7782f6f5bf367d715d4c49c2b1
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-08-10 18:59:44 +0900

    feat : Add EIP, SG to Worker Node

--- worker_nodes.tf
+++ worker_nodes.tf
@@ -14,11 +14,6 @@ resource "aws_eks_node_group" "eks_nodes_t2" {
     min_size     = var.eks_node_sc_min_size
     max_size     = var.eks_node_sc_max_size
   }
-
-  remote_access {
-    ec2_ssh_key               = var.eks_node_key_name
-    source_security_group_ids = [aws_security_group.sg_eks_cluster.id]
-  }
   
   depends_on = [
     aws_iam_role_policy_attachment.eks_node_policy,
@@ -27,4 +22,47 @@ resource "aws_eks_node_group" "eks_nodes_t2" {
   tags = {
     "Name" = "${aws_eks_cluster.eks_cluster.name}-eks-t2-medium-Node"
   }
+}
+
+#####################
+# Worker node lookup
+#####################
+data "aws_instances" "workers" {
+  instance_tags = {
+    "eks:nodegroup-name" = var.eks_node_group_name
+  }
+
+  depends_on = [aws_eks_node_group.eks_nodes_t2]
+}
+
+data "aws_instance" "workers" {
+  count = length(data.aws_instances.workers)
+
+  instance_id = element(data.aws_instances.workers.ids, count.index)
+}
+
+###########################################
+# Adding a Security Group to a Worker Node
+###########################################
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  count = length(data.aws_instance.workers)
+  security_group_id = aws_security_group.sg_allow_all.id
+  network_interface_id = element(data.aws_instance.workers.*.network_interface_id, count.index)
+}
+
+#####################################
+# Elastic ip lookup for worker nodes
+#####################################
+data "aws_eip" "worker_node_eip" {
+  tags = {
+    Name = var.worker_node_eip_name
+  }
+}
+
+##################################
+# EIP connections to worker nodes
+##################################
+resource "aws_eip_association" "worker_node_eip_assoc" {
+  instance_id   = data.aws_instance.workers[0].id
+  allocation_id = data.aws_eip.worker_node_eip.id
 }
