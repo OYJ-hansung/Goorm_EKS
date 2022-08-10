cbdf30bf2c564457182759f73ca59367eff13039
BoGyeong <76933244+boookk@users.noreply.github.com>
2022-08-10 16:40:33 +0900

    feat : Additional sg to allow all inbound

--- sg.tf
+++ sg.tf
@@ -1,4 +1,6 @@
-# create security group
+######################################
+# Creating an eks-only security group
+######################################
 resource "aws_security_group" "sg_eks_cluster" {
   name        = var.sg_eks_cluster_name
   description = var.sg_eks_cluster_description
@@ -18,11 +20,42 @@ resource "aws_security_group" "sg_eks_cluster" {
 
 
 # add ingress rule of security group
-resource "aws_security_group_rule" "sg_eks_cluster_ingress_workstation_https" {
+resource "aws_security_group_rule" "sg_eks_cluster_ingress" {
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 0
   protocol          = "-1"
   security_group_id = aws_security_group.sg_eks_cluster.id
+  to_port           = 0
+  type              = "ingress"
+}
+
+##################################################
+# Create a security group that allows all inbound
+##################################################
+resource "aws_security_group" "sg_allow_all" {
+  name        = var.sg_allow_all
+  description = var.sg_allow_all_description
+  vpc_id      = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_allow_all
+  }
+}
+
+
+# add ingress rule of security group
+resource "aws_security_group_rule" "sg_eks_allow_all" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.sg_allow_all.id
   to_port           = 0
   type              = "ingress"
 }
